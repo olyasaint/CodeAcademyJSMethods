20e67977c5054e72edd699fa5562c1c7992e7bbf
olyasaint <olya@ostryk.com>
2014-11-24 11:59:35 -0800

    Create Array.script.js

---
+++ Array.script.js
@@ -0,0 +1,14 @@
+function Person (name, age) {
+    this.name = name;
+    this.age = age;
+}
+var family = new Array ();
+family[0] = new Person("alice", 40);
+family[1] = new Person("bob", 42);
+family[2] = new Person("michelle", 8);
+family[3] = new Person("timmy",6)
+
+// loop through our new array
+for (i=0; i<family.length; i++) {
+    console.log(family[i].name);
+};
